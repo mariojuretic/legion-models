7bb18a25bb9cb7b56df78476406e02afdc724c59
Mario Juretić <mario@weblio.hr>
2023-09-20 11:44:48 +0200

    configured remote patterns and created helper for image urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.sanity.io",
+        protocol: "https",
+      },
+    ],
+  },
   experimental: {
     urlImports: ["https://themer.sanity.build/"],
   },
