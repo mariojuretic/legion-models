7f713e99f24aa599b1f7b1ef79a49e465c6f2e2f
Mario Juretić <mario@weblio.hr>
2023-09-21 08:59:27 +0200

    added responsive menu with zustand state

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import Link from "next/link";
 import localFont from "next/font/local";
-import { Bars3BottomRightIcon } from "@heroicons/react/24/outline";
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+import { useMenuStore } from "@/store/MenuStore";
 
 const drukWide = localFont({ src: "../fonts/DrukWide-Bold.ttf" });
 
 export default function Header() {
+  const [isOpen, openMenu, closeMenu] = useMenuStore((state) => [
+    state.isOpen,
+    state.openMenu,
+    state.closeMenu,
+  ]);
+
   return (
-    <header className="sticky top-0 flex items-center justify-between bg-black lg:bg-transparent">
+    <header className="sticky top-0 z-50 flex items-center justify-between bg-black lg:bg-transparent">
       <div className="p-4 lg:w-80 lg:p-8">
         <h1
-          className={`${drukWide.className} text-3xl leading-none lg:text-4xl`}
+          className={`${drukWide.className} text-3xl leading-none lg:text-4xl lg:leading-none`}
         >
           <Link href="/models" className="inline-block">
             LEGION
@@ -17,8 +27,12 @@ export default function Header() {
         </h1>
       </div>
 
-      <button className="p-4 lg:hidden">
-        <Bars3BottomRightIcon className="h-[30px] w-[30px]" />
+      <button className="p-4 lg:hidden" onClick={isOpen ? closeMenu : openMenu}>
+        {isOpen ? (
+          <XMarkIcon className="h-[30px] w-[30px]" />
+        ) : (
+          <Bars3BottomRightIcon className="h-[30px] w-[30px]" />
+        )}
       </button>
 
       {/* SearchBar */}
--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,8 +1,24 @@
+"use client";
+
 import Link from "next/link";
 
+import { useMenuStore } from "@/store/MenuStore";
+
 export default function Menu() {
+  const [isOpen, closeMenu] = useMenuStore((state) => [
+    state.isOpen,
+    state.closeMenu,
+  ]);
+
   return (
-    <nav className="sticky top-[100px] hidden w-80 flex-col items-start self-start p-8 text-sm font-light uppercase tracking-widest lg:flex">
+    <nav
+      className={`flex flex-col text-sm font-light uppercase tracking-widest lg:sticky lg:top-[100px] lg:w-80 lg:items-start lg:self-start lg:p-8 ${
+        isOpen
+          ? "max-lg:fixed max-lg:bottom-0 max-lg:left-0 max-lg:right-0 max-lg:top-[62px] max-lg:z-50 max-lg:items-center max-lg:justify-center max-lg:bg-black max-lg:p-4"
+          : "max-lg:hidden"
+      }`}
+      onClick={closeMenu}
+    >
       <Link href="/about">About</Link>
       <Link href="/models">Models</Link>
       <Link href="/new-faces">New Faces</Link>
