ea8d69087a14f1bc23879dc459b7261dea53b047
Mario Juretić <mario@weblio.hr>
2023-09-21 06:53:49 +0200

    added ModelsList component and fetching models data

--- app/(default)/models/page.tsx
+++ app/(default)/models/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <div>Models Page</div>;
+import { groq } from "next-sanity";
+
+import ModelsList from "@/components/ModelsList";
+import { client } from "@/lib/sanity.client";
+
+const query = groq`
+  *[_type == "model" && category == "models" && hidden == false]
+`;
+
+export default async function Page() {
+  const models: ModelDocument[] = await client.fetch(query);
+
+  return (
+    <main className="grow self-start p-8">
+      <div className="text-sm font-light uppercase tracking-widest">
+        {models.length === 0 && <p>No models found.</p>}
+
+        {models.length > 0 && <ModelsList models={models} />}
+      </div>
+    </main>
+  );
 }
---
+++ components/ModelsList.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+import urlFor from "@/lib/urlFor";
+
+function ModelCard({ model }: { model: ModelDocument }) {
+  return (
+    <li className="group flex cursor-pointer flex-col space-y-4">
+      <div className="relative aspect-[2/3] w-full overflow-hidden bg-neutral-900 transition-colors group-hover:bg-neutral-800">
+        {model.thumbnail?.default && (
+          <Image
+            src={urlFor(model.thumbnail.default).url()}
+            alt={model.thumbnail.default.description || model.name}
+            fill
+            className={`z-10 object-cover object-center ${
+              !model.thumbnail.hover
+                ? "transition-transform duration-300 group-hover:scale-105"
+                : ""
+            }`}
+          />
+        )}
+
+        {model.thumbnail?.default && model.thumbnail.hover && (
+          <Image
+            src={urlFor(model.thumbnail.hover).url()}
+            alt={model.thumbnail.hover.description || model.name}
+            fill
+            className="z-20 object-cover object-center opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          />
+        )}
+      </div>
+
+      <p className="text-center">{model.name}</p>
+    </li>
+  );
+}
+
+export default function ModelsList({ models }: { models: ModelDocument[] }) {
+  return (
+    <ul className="grid grid-cols-6 gap-8 pr-16">
+      {models.map((model) => (
+        <ModelCard key={model._id} model={model} />
+      ))}
+    </ul>
+  );
+}
