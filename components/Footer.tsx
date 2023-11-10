0274b452cd9471a579049815ee7c31f730ce1f28
Mario Juretić <mario@weblio.hr>
2023-11-10 16:51:39 +0100

    fixed left offset in footer due to smaller nav menu

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="brand-text flex flex-wrap items-center gap-x-4 p-4 lg:ml-80 lg:flex-row-reverse lg:justify-center lg:p-8">
+    <footer className="brand-text flex flex-wrap items-center gap-x-4 p-4 lg:ml-60 lg:flex-row-reverse lg:justify-center lg:p-8">
       <ul className="flex flex-wrap items-center gap-x-4">
         <li className="flex">
           <Link href="/privacy-policy" className="hover:font-bold">
