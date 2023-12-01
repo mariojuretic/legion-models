3691481a9bdf44bc4558f86f8a4e80259c17cb7a
Mario Juretić <mario@weblio.hr>
2023-12-01 11:06:49 +0100

    make sure first slide in carousel is a single image

--- lib/generateSlides.ts
+++ lib/generateSlides.ts
@@ -1,35 +1,42 @@
 export default function generateSlides(images: ImageType[]) {
   const slides: ImageType[][] = [];
 
+  let firstImage = true;
   let storedImage;
 
   // Loop through all images
   for (const image of images) {
-    // Orientation of current image
-    const orientation =
-      image.dimensions!.aspectRatio < 1 ? "portrait" : "landscape";
-
-    // Check if previous image was portrait oriented and stored
-    if (storedImage) {
-      // Check orientation of current image
-      if (orientation == "portrait") {
-        // Merge into a slide with previous image
-        slides.push([storedImage, image]);
-      } else {
-        // Add previous and current image as separate slides
-        slides.push([storedImage]);
-        slides.push([image]);
-      }
-      // Clear stored image after adding
-      storedImage = null;
+    if (firstImage) {
+      // Add first image as a separate slide
+      slides.push([image]);
+      firstImage = false;
     } else {
-      // Check orientation of current image
-      if (orientation == "portrait") {
-        // Store portrait image and check next one
-        storedImage = image;
+      // Orientation of current image
+      const orientation =
+        image.dimensions!.aspectRatio < 1 ? "portrait" : "landscape";
+
+      // Check if previous image was portrait oriented and stored
+      if (storedImage) {
+        // Check orientation of current image
+        if (orientation == "portrait") {
+          // Merge into a slide with previous image
+          slides.push([storedImage, image]);
+        } else {
+          // Add previous and current image as separate slides
+          slides.push([storedImage]);
+          slides.push([image]);
+        }
+        // Clear stored image after adding
+        storedImage = null;
       } else {
-        // Add landscape image as a new slide
-        slides.push([image]);
+        // Check orientation of current image
+        if (orientation == "portrait") {
+          // Store portrait image and check next one
+          storedImage = image;
+        } else {
+          // Add landscape image as a new slide
+          slides.push([image]);
+        }
       }
     }
   }
