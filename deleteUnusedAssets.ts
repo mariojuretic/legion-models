b671c1ba2da6092b7a9a7b0420535b59adf7e796
Mario Juretić <mario@weblio.hr>
2024-01-20 09:48:02 +0100

    added sanity script to delete unused video assets from mux

--- deleteUnusedAssets.ts
+++ deleteUnusedAssets.ts
@@ -1,37 +1,66 @@
-import { type Transaction, groq } from "next-sanity";
+import { groq } from "next-sanity";
 
 import { writeClient } from "@/lib/sanity.client";
 
+type Asset = {
+  _type: "sanity.imageAsset" | "sanity.fileAsset" | "mux.videoAsset";
+  _id: string;
+  assetId: string;
+};
+
 const query = groq`
-  *[_type in ["sanity.imageAsset", "sanity.fileAsset"]] {
+  *[_type in ["sanity.imageAsset", "sanity.fileAsset", "mux.videoAsset"]] {
+    _type,
     _id,
+    assetId,
     "refs": count(*[references(^._id)])
-  }[refs == 0]._id
+  }[refs == 0]{
+    _type,
+    _id,
+    assetId
+  }
 `;
 
-writeClient
-  .fetch(query)
-  .then((ids: string[]) => {
-    if (!ids || ids.length === 0) {
+const deleteUnusedAssets = async () => {
+  try {
+    // Fetch unused assets from database
+    const assets: Asset[] = await writeClient.fetch(query);
+
+    if (!assets || assets.length === 0) {
       console.log("No assets to delete.");
       return;
     }
 
-    console.log(`Deleting ${ids.length} assets...`);
+    console.log(`Deleting ${assets.length} assets...`);
 
-    return ids
-      .reduce(
-        (trx: Transaction, id: string) => trx.delete(id),
-        writeClient.transaction(),
-      )
+    // Delete all assets from Sanity
+    await assets
+      .reduce((trx, asset) => trx.delete(asset._id), writeClient.transaction())
       .commit();
-  })
-  .then(() => console.log("Done!"))
-  .catch((err: Error) => {
-    if (err.message.includes("Insufficient permissions")) {
-      console.log(err.message);
-      console.log(`Did you forget to pass "--with-user-token"?`);
-    } else {
-      console.log(err.stack);
-    }
-  });
+
+    // Delete video assets from Mux
+    await Promise.all(
+      assets
+        .filter((asset) => asset._type === "mux.videoAsset")
+        .map((asset) =>
+          fetch(`https://api.mux.com/video/v1/assets/${asset.assetId}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Basic " +
+                btoa(
+                  `${process.env.MUX_TOKEN_ID}:${process.env.MUX_TOKEN_SECRET}`,
+                ),
+            },
+          }),
+        ),
+    );
+
+    console.log("Done!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+deleteUnusedAssets();
