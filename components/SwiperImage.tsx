779627955e6b5f22e210908ac556aeaf1aa88a2d
mariojuretic <mario@weblio.hr>
2024-03-27 20:00:44 +0100

    switched to default img tags for better responsiveness

--- components/SwiperImage.tsx
+++ components/SwiperImage.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
-
 import urlFor from "@/lib/urlFor";
 
 type Props = {
@@ -16,53 +13,6 @@ export default function SwiperImage({
   altText,
   alignment = "center",
 }: Props) {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  const [containerWidth, setContainerWidth] = useState<number>();
-  const [containerHeight, setContainerHeight] = useState<number>();
-
-  const [imageWidth, setImageWidth] = useState<number>();
-  const [imageHeight, setImageHeight] = useState<number>();
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (!containerRef.current) return;
-
-      setContainerWidth(containerRef.current.clientWidth);
-      setContainerHeight(containerRef.current.clientHeight);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    if (!containerWidth || !containerHeight) return;
-    if (!image.dimensions) return;
-
-    // Upscale before scaling down to fill container...
-    let newImageWidth = image.dimensions.width * 1000;
-    let newImageHeight = image.dimensions.height * 1000;
-
-    // Limit image width to container...
-    if (newImageWidth > containerWidth) {
-      const resizeFactor = containerWidth / newImageWidth;
-      newImageWidth = Math.floor(newImageWidth * resizeFactor);
-      newImageHeight = Math.floor(newImageHeight * resizeFactor);
-    }
-
-    // Limit image height to container...
-    if (newImageHeight > containerHeight) {
-      const resizeFactor = containerHeight / newImageHeight;
-      newImageWidth = Math.floor(newImageWidth * resizeFactor);
-      newImageHeight = Math.floor(newImageHeight * resizeFactor);
-    }
-
-    setImageWidth(newImageWidth);
-    setImageHeight(newImageHeight);
-  }, [containerWidth, containerHeight, image.dimensions]);
-
   const justifyContent =
     alignment === "start"
       ? "flex-start"
@@ -72,37 +22,14 @@ export default function SwiperImage({
 
   return (
     <div
-      ref={containerRef}
       className="relative flex h-full w-full items-center"
       style={{ justifyContent }}
     >
-      {containerWidth && containerHeight && imageWidth && imageHeight && (
-        <>
-          <Image
-            src={urlFor(image).url()}
-            alt={altText}
-            width={imageWidth}
-            height={imageHeight}
-          />
-
-          {image.source && (
-            <p
-              className="brand-text absolute"
-              style={{
-                bottom: (containerHeight - imageHeight) / 2 - 13,
-                right:
-                  alignment === "start"
-                    ? containerWidth - imageWidth
-                    : alignment === "end"
-                      ? 0
-                      : (containerWidth - imageWidth) / 2,
-              }}
-            >
-              {image.source}
-            </p>
-          )}
-        </>
-      )}
+      <img
+        src={urlFor(image).url()}
+        alt={altText}
+        className="max-h-full max-w-full"
+      />
     </div>
   );
 }
--- components/SwiperImageSlider.tsx
+++ components/SwiperImageSlider.tsx
@@ -19,41 +19,39 @@ export default function SwiperImageSlider({
   return (
     <section className="relative h-full w-full">
       <div className={`h-full w-full ${addVerticalPadding ? "p-8" : "px-8"}`}>
-        <div className="h-full w-full pb-[13px]">
-          <Swiper className="h-full w-full">
-            {slides.map((slide) => (
-              <SwiperSlide key={slide[0].asset._ref}>
-                {slide.length === 1 && (
-                  <div className="grid h-full w-full grid-cols-1">
-                    <div className="h-full w-full border border-red-500">
-                      <SwiperImage image={slide[0]} altText={imagesAltText} />
-                    </div>
+        <Swiper className="h-full w-full">
+          {slides.map((slide) => (
+            <SwiperSlide key={slide[0].asset._ref}>
+              {slide.length === 1 && (
+                <div className="grid h-full w-full grid-cols-1">
+                  <div className="h-full w-full overflow-hidden border border-red-500">
+                    <SwiperImage image={slide[0]} altText={imagesAltText} />
                   </div>
-                )}
+                </div>
+              )}
 
-                {slide.length === 2 && (
-                  <div className="grid h-full w-full grid-cols-2 gap-x-4">
-                    <div className="h-full w-full border border-green-500">
-                      <SwiperImage
-                        image={slide[0]}
-                        altText={imagesAltText}
-                        alignment="end"
-                      />
-                    </div>
-
-                    <div className="h-full w-full border border-blue-500">
-                      <SwiperImage
-                        image={slide[1]}
-                        altText={imagesAltText}
-                        alignment="start"
-                      />
-                    </div>
+              {slide.length === 2 && (
+                <div className="grid h-full w-full grid-cols-2 gap-x-4">
+                  <div className="h-full w-full overflow-hidden border border-green-500">
+                    <SwiperImage
+                      image={slide[0]}
+                      altText={imagesAltText}
+                      alignment="end"
+                    />
                   </div>
-                )}
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
+
+                  <div className="h-full w-full overflow-hidden border border-blue-500">
+                    <SwiperImage
+                      image={slide[1]}
+                      altText={imagesAltText}
+                      alignment="start"
+                    />
+                  </div>
+                </div>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
